4c2e2babe05345b16347214669368cbefb1b0661
Sami Suteria <samisuteria@users.noreply.github.com>
2015-12-20 21:17:47 -0800

    Create UIView.swift

---
+++ UIView.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class SomeView: UIView {
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
